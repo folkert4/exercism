9b64583032307b7f11e2ad2a932eaeb8a129cdbb
folkert4 <folkertjw!gmail.com>
2017-04-13 14:18:39 -0400

    remove uneeded loop.

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -7,7 +7,7 @@ var checkPan = function (sentence) {
         a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0,
         n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0
     };
-    if (sentence.length > 0) {
+    if (sentence.length >= 26) {
         for (var i = 0; i < sentence.length; i++) {
             try {
                 alphabet[sentence[i]] += 1;
@@ -16,12 +16,11 @@ var checkPan = function (sentence) {
                 //ignore if not in the alphabet (underscores and special chars)
             }
         }
-        var check = [];
         for (var key in alphabet) {
-            check.push(alphabet[key]);
+            if (alphabet[key] === 0) { return false; }
         }
-        return check.every(x => x > 0);
     } else { return false; }
+    return true;
 };
 
 Pangram.prototype.isPangram = function () {
